a15495b1c8d680f8313c24a4916520a6e1ab3ce5
Damian Staszewski <damian@stachuofficial.pl>
2018-09-04 15:44:35 +0200

    fix(*): a little bit fix for compilation

--- chat/src/main/java/glitch/chat/ChannelCache.java
+++ chat/src/main/java/glitch/chat/ChannelCache.java
@@ -2,11 +2,12 @@ package glitch.chat;
 
 import com.google.common.collect.SetMultimap;
 import com.google.common.collect.MultimapBuilder;
+import glitch.kraken.json.Channel;
 
 public class ChannelCache {
     private final SetMultimap<Channel, Chat> chats = MultimapBuilder.linkedHashKeys().hashSetValues().build();
 
     public Chat getChatRoom(String channel, String roomId) {
-
+        return null;
     }
 }
--- chat/src/main/java/glitch/chat/Chat.java
+++ chat/src/main/java/glitch/chat/Chat.java
@@ -1,8 +1,8 @@
 package glitch.chat;
 
-import glitch.chat.MessageInterface;
+import glitch.kraken.json.Channel;
 import java.util.LinkedHashSet;
-import java.util.List;
+import java.util.Set;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -13,9 +13,9 @@ public class Chat {
     @Getter(AccessLevel.NONE)
     private final MessageInterface tmi;
     private final Channel channel;
-    private final Set<Chat.User> chatUsers = new LinkedHashSet<Chat.User>();
+    private final Set<User> chatUsers = new LinkedHashSet<>();
 
-    public static class User {
+    public static interface User {
         long getId();
         String getUsername();
         String getDisplayName();
