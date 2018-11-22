571815ed277dcc97f38602a9f3e119840369abd2
Damian Staszewski <damian@stachuofficial.pl>
2018-11-22 02:23:39 +0100

    ci(travis): fixing javadoc deploying

--- javadoc.sh
+++ javadoc.sh
@@ -6,6 +6,8 @@ JAVADOC_REPO=https://github.com/GlitchLib/docs.git
 
 git clone ${JAVADOC_REPO} ./build/javadocs
 
+mkdir ./build/javadocs/${TRAVIS_TAG}
+
 for PROJECT in "auth" "chat" "core" "docs" "helix" "kraken" "pubsub"; do
     DIRECTORY=${PROJECT}/build/docs/javadoc
     if [[ PROJECT == "docs" ]]; then
@@ -14,13 +16,15 @@ for PROJECT in "auth" "chat" "core" "docs" "helix" "kraken" "pubsub"; do
 
     JAVADOC_DESTINATION=build/javadocs/${TRAVIS_TAG}/${PROJECT}
 
-    cp -arf ${DIRECTORY} ${JAVADOC_DESTINATION}
+    cp -ar ${DIRECTORY} ${JAVADOC_DESTINATION}
 done
 
 cd ./build/javadocs
 
-ln -sfn ./${TRAVIS_TAG} ./latest
 cp ./latest/index.html ./${TRAVIS_TAG}/index.html
+ln -sfn ./${TRAVIS_TAG} ./latest
 
+git add .
 git commit -m "Build release: ${TRAVIS_TAG}"
 git push origin master
+
