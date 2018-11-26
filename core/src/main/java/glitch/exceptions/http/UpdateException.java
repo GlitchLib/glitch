bb5e48c38698acff98fddcc9b63cb8ab76c01b74
Damian Staszewski <damian@stachuofficial.pl>
2018-11-27 00:39:49 +0100

    feat(core/api): add UpdateException for requests body

---
+++ core/src/main/java/glitch/exceptions/http/UpdateException.java
@@ -0,0 +1,9 @@
+package glitch.exceptions.http;
+
+import glitch.exceptions.GlitchException;
+
+public class UpdateException extends GlitchException {
+    public UpdateException(String message) {
+        super(message);
+    }
+}
