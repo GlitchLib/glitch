0bc96ffb58755e4c864f85c24dada141a42b9aed
Damian Staszewski <damian@stachuofficial.tv>
2019-02-11 06:05:57 +0100

    revert(4502d3b1): refactor(pubsub): Updating PubSub

---
+++ pubsub/src/main/java/glitch/pubsub/object/adapters/MessageTypeAdapter.java
@@ -0,0 +1,19 @@
+package glitch.pubsub.object.adapters;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import glitch.pubsub.object.enums.MessageType;
+import java.io.IOException;
+
+public class MessageTypeAdapter extends TypeAdapter<MessageType> {
+    @Override
+    public void write(JsonWriter out, MessageType value) throws IOException {
+        out.value(value.name());
+    }
+
+    @Override
+    public MessageType read(JsonReader in) throws IOException {
+        return MessageType.valueOf(in.nextString());
+    }
+}
