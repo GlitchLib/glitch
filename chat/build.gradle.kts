e646d12fa6c1f01911aef20dbe86b217925eafd7
Damian Staszewski <damian@stachuofficial.tv>
2019-02-24 23:32:41 +0100

    build(gradle): reduce dependency

--- chat/build.gradle.kts
+++ chat/build.gradle.kts
@@ -1,7 +1,5 @@
 dependencies {
     compileOnly(project(":core"))
-    compileOnly(project(":kraken"))
 
     testCompile(project(":core"))
-    testCompile(project(":kraken"))
 }
--- pubsub/build.gradle.kts
+++ pubsub/build.gradle.kts
@@ -1,8 +1,4 @@
 dependencies {
     compileOnly(project(":core"))
-    compileOnly(project(":kraken"))
-    compileOnly(project(":helix"))
     testCompile(project(":core"))
-    testCompile(project(":kraken"))
-    testCompile(project(":helix"))
 }
